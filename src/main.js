63e7b2f76e429390cd58ab1f26c620ef46052787
hannah <hma0910@gmail.com>
2022-11-27 13:17:19 -0600

    check answe3r

--- src/main.js
+++ src/main.js
@@ -1,13 +1,19 @@
+'use strict';
+
 // tip: docs @ https://docs.urturn.app/docs/API/backend#functions
 
 const MoveTypes = Object.freeze({
   StartGame: 'start_game',
   Guess: 'guess',
   NewRound: 'new_round'
+});
+
+const Answer = Object.freeze({
+  dont_stop_believing_clip: 'lonely world she',
+  stiches: 'no ones ever'
 })
 
 function onRoomStart(roomState) {
-  const { logger } = roomState;
   return {
     state: {
       rounds: [],
@@ -19,34 +25,28 @@ function onRoomStart(roomState) {
 }
 
 function onPlayerJoin(player, roomState) {
-  const { logger } = roomState
-  logger.info('Join called with:', { player, roomState })
-  logger.warn('TODO: implement how to change the roomState when a player joins')
+  const { logger } = roomState;
+  logger.info('Join called with:', { player, roomState });
+  logger.warn('TODO: implement how to change the roomState when a player joins');
   return {}
 }
 
 function onPlayerQuit(player, roomState) {
-  const { logger } = roomState
-  logger.info('Quit called with:', { player, roomState })
-  logger.warn('TODO: implement how to change the roomState when a player quits the room')
+  const { logger } = roomState;
+  logger.info('Quit called with:', { player, roomState });
+  logger.warn('TODO: implement how to change the roomState when a player quits the room');
   return {}
-}
-
-
-
-function getNextSong(){
-  
 }
 
 function getNewRound(songs, currentSongIndex) {
   return {
-  song: state.songs[state.currentSongIndex], //change to state.songs[state.currentSongIndex]
+  song: songs[currentSongIndex], //change to state.songs[state.currentSongIndex]
   playerPoints: {},
   }
 }
 
 function onPlayerMove(player, move, roomState) {
-  const { logger, state } = roomState
+  const { logger, state } = roomState;
   const { type, data } = move;
   const { rounds, totalPoints, songs, currentSongIndex } = state;
 
@@ -55,20 +55,17 @@ function onPlayerMove(player, move, roomState) {
   switch (type) {
     case MoveTypes.StartGame:
       //shuffle songs
-      songs = songs.sort((a, b) => 0.5 - Math.random());
+      state.songs = songs.sort((a, b) => 0.5 - Math.random());
       rounds.push(getNewRound(songs, currentSongIndex));
       totalPoints[player.id] = 0;
       return { joinable: false, state: state }
     case MoveTypes.Guess:
-      logger.info("DATA: ", data)
-      if (data === "a b c") {
+      if (data === Answer[songs[currentSongIndex]]) {
         rounds[currentRound].playerPoints[player.id] = 100;
         totalPoints[player.id] = Object.keys(rounds).reduce((prev, curr) => {
           Object.keys(curr).map(key => prev[key] += curr[key]);
-        }, {})
+        }, {});
         return { state: state }
-      } else {
-
       }
       break;
     case MoveTypes.NewRound:
@@ -83,9 +80,11 @@ function onPlayerMove(player, move, roomState) {
 
 // Export these functions so UrTurn runner can run these functions whenever the associated event
 // is triggered. Follow an example flow of events: https://docs.urturn.app/docs/Introduction/Flow-Of-Simple-Game
-export default {
+var main = {
   onRoomStart,
   onPlayerJoin,
   onPlayerQuit,
   onPlayerMove,
 };
+
+module.exports = main;
