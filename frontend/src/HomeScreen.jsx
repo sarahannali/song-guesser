adf48826f90b9846bd79a9a770102fd5d80efe61
hannah <hma0910@gmail.com>
2022-11-27 22:31:19 -0600

    changes to home screen

--- frontend/src/HomeScreen.jsx
+++ frontend/src/HomeScreen.jsx
@@ -9,34 +9,61 @@ import {
 function HomeScreen({ players }) {
   return (
     <Stack
-      spacing={2}
+      spacing={5}
       sx={{
         height: '100vh',
         display: 'flex',
         justifyContent: 'center',
         alignItems: 'center',
+
       }}
     >
       <Typography
         sx={{
-          fontSize: '3em',
+          fontSize: '1.5em',
           color: 'white',
           fontFamily: 'Bungee',
+          // position: 'relative',
+          // bottom: '295px',
+          // webkitTextStroke: '5px #666666',
+          textShadow: '-5px 9px 7px #464646',
         }}
       >
-        SONG GUESSER
+        welcome to song guessing!
       </Typography>
       <Button
         variant="contained"
         size="large"
         onClick={() => client.makeMove({ type: 'start_game' })}
+        sx={{
+          height: '150px',
+          width: '450px',
+          // position: 'relative',
+          // bottom: '230px',
+          backgroundColor: '#124E86',
+          fontFamily: 'Bungee',
+          fontSize: '1.2em',
+        }}
       >
         START
       </Button>
-      {players.length > 1 && (
-      <Paper>
-        <Stack padding={1} sx={{ minWidth: '100px' }}>
-          <Typography color="text.primary">Players</Typography>
+      <Paper sx={{
+        height: '500px',
+        width: '340px',
+        padding: '50px',
+      }}
+      >
+        <Stack justifyContent="center" alignItems="center" sx={{ minWidth: '100px' }}>
+          <Typography
+            sx={{
+              fontFamily: 'Bungee',
+              fontSize: '1.35em',
+            }}
+            color="text.primary"
+          >
+            Players
+
+          </Typography>
           <List dense disablePadding padding={0}>
             {players.map((player, ind) => (
               <ListItem dense disablePadding key={player.id}>
@@ -46,7 +73,7 @@ function HomeScreen({ players }) {
           </List>
         </Stack>
       </Paper>
-      )}
+
     </Stack>
   );
 }
